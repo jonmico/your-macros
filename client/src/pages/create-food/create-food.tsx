9af83021ffe4145b88e04331dacdee4e676a4042
Jon Mico <jonmico@gmail.com>
2023-11-24 00:31:12 -0500

    Start working on form for create food page.

--- client/src/pages/create-food/create-food.tsx
+++ client/src/pages/create-food/create-food.tsx
@@ -1,7 +1,38 @@
+import styles from './create-food.module.css';
+
 export default function CreateFood() {
+  function handleSubmit(evt: React.FormEvent<HTMLFormElement>) {
+    evt.preventDefault();
+    console.log('Form submitted!');
+  }
   return (
     <div>
-      <h1>CREATE FOOD</h1>
+      <h2>Create Food</h2>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <div>
+          <label htmlFor='name'>Name</label>
+          <input type='text' />
+        </div>
+        <div>
+          <label htmlFor='servingSize'>Serving Size</label>
+          <input type='text' />
+        </div>
+        <div>
+          <label htmlFor='carbs'>Carbs</label>
+          <input type='number' min={0} max={999} />
+        </div>
+        <div>
+          <label htmlFor='fats'>Fats</label>
+          <input type='number' min={0} max={999} />
+        </div>
+        <div>
+          <label htmlFor='protein'>Protein</label>
+          <input type='number' min={0} max={999} />
+        </div>
+        <div>
+          <button>Submit</button>
+        </div>
+      </form>
     </div>
   );
 }
