75486dd320592c7c2a9610e8185c9ebd9a992095
Jon Mico <jonmico@gmail.com>
2023-11-23 23:27:53 -0500

    Add some styling to navbar.

--- client/src/components/navbar/navbar.tsx
+++ client/src/components/navbar/navbar.tsx
@@ -1,21 +1,37 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { MdOutlineSpaceDashboard } from 'react-icons/md';
+import { GiMeal } from 'react-icons/gi';
+import { IoIosJournal } from 'react-icons/io';
+import { FaPencil } from 'react-icons/fa6';
 import styles from './navbar.module.css';
 
 export default function Navbar() {
   return (
     <nav className={styles.navbar}>
       <ul className={styles.navList}>
-        <li className={styles.navItem}>
-          <Link to={'/dashboard'}>Dashboard</Link>
+        <li>
+          <NavLink to={'/dashboard'} className={styles.navItem}>
+            <MdOutlineSpaceDashboard className={styles.icon} />
+            <span>Dashboard</span>
+          </NavLink>
         </li>
-        <li className={styles.navItem}>
-          <Link to={'/add-meal'}>Add Meal</Link>
+        <li>
+          <NavLink to={'/add-meal'} className={styles.navItem}>
+            <GiMeal className={styles.icon} />
+            <span>Add Meal</span>
+          </NavLink>
         </li>
-        <li className={styles.navItem}>
-          <Link to={'/logs'}>Logs</Link>
+        <li>
+          <NavLink to={'/logs'} className={styles.navItem}>
+            <IoIosJournal className={styles.icon} />
+            <span>Logs</span>
+          </NavLink>
         </li>
-        <li className={styles.navItem}>
-          <Link to={'create-food'}>Create Food</Link>
+        <li>
+          <NavLink to={'create-food'} className={styles.navItem}>
+            <FaPencil className={styles.icon} />
+            <span>Create Food</span>
+          </NavLink>
         </li>
       </ul>
     </nav>
