87b05a158249451fdddee56f4717c410cf876e59
Jon Mico <jonmico@gmail.com>
2023-11-27 18:26:23 -0500

    Test some different error handling for front-end.

--- client/src/services/food-api.ts
+++ client/src/services/food-api.ts
@@ -7,24 +7,13 @@ export async function createFood(food: IFood) {
     body: JSON.stringify({ food }),
   });
 
-  if (!res.ok) {
-    throw new Error(`Post request failed with error code: ${res.status}`);
-  }
-
   const data = await res.json();
 
   return data;
 }
 
 export async function getFoods() {
-  const res = await fetch('/api/foods', {
-    method: 'get',
-    headers: { 'content-type': 'application/json' },
-  });
-
-  if (!res.ok) {
-    throw new Error(`Something went wrong: ${res.status} error.`);
-  }
+  const res = await fetch('/api/foods');
 
   const data = await res.json();
 
