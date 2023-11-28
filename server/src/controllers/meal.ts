79f48683cd85992ebe42b810a6c120169e5df603
Jon Mico <jonmico@gmail.com>
2023-11-28 15:46:42 -0500

    Add function to get a meal by id.

--- server/src/controllers/meal.ts
+++ server/src/controllers/meal.ts
@@ -1,9 +1,26 @@
 import { NextFunction, Request, Response } from 'express';
 import IMeal from '../types/meal';
 import Meal from '../models/meal';
+import AppError from '../app-error';
 
 interface IBody {
   meal: IMeal;
+}
+
+export async function getMeal(req: Request, res: Response, next: NextFunction) {
+  try {
+    const { id } = req.params;
+
+    const meal = await Meal.findById(id);
+
+    if (!meal) {
+      throw new AppError('Meal not found.', 404);
+    }
+
+    res.json({ meal });
+  } catch (err) {
+    next(err);
+  }
 }
 
 export async function createMeal(
--- server/src/routes/meal.ts
+++ server/src/routes/meal.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import { createMeal } from '../controllers/meal';
+import { createMeal, getMeal } from '../controllers/meal';
 
 const router = Router();
 
 router.post('/new', createMeal);
+
+router.get('/:id', getMeal);
 
 export default router;
