176e5470e045e4798deea747786f6e992b934688
Jon Mico <jonmico@gmail.com>
2023-12-05 14:00:19 -0500

    Very early FoodSearchListItem. I have to go but I'll fix this up tonight.

--- client/src/components/food-search-list-item/food-search-list-item.tsx
+++ client/src/components/food-search-list-item/food-search-list-item.tsx
@@ -1,10 +1,9 @@
-import { IFood } from "../../types/food"
+import { IFood } from '../../types/food';
 
 interface FoodSearchListItemProps {
   food: IFood;
 }
-export default function FoodSearchListItem() {
-  return (
-
-  )
+export default function FoodSearchListItem(props: FoodSearchListItemProps) {
+  const { brand, name, servingSize } = props.food;
+  return <li>{brand}</li>;
 }
