8b7749151c6aa3161b4ce72ad8ed2e810cc88649
Jon Mico <jonmico@gmail.com>
2023-12-10 20:47:29 -0500

    Pretty much copy paste meal interface from server over to client. Might want to change frontend meal so might be worth to have them separated.

---
+++ client/src/types/meal.ts
@@ -0,0 +1,12 @@
+import { IFood } from './food';
+
+export default interface IMeal {
+  mealComponents: { food: IFood; servings: number }[];
+  name: string;
+  calories: number;
+  macros: {
+    carbs: number;
+    fat: number;
+    protein: number;
+  };
+}
