42380c64ec207131c0fd68610eea2f8535b2dadc
Jon Mico <jonmico@gmail.com>
2023-12-15 18:02:08 -0500

    Try something new for api calls.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const API_URL = import.meta.env.API_URL || 'http://localhost:3000';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: API_URL,
         secure: false,
       },
     },
