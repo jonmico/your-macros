4c5ddf8ae5418caab58b9d1e8f65fd296cfc31a2
Jon Mico <jonmico@gmail.com>
2023-12-15 00:11:23 -0500

    Implement add to meal functionality of '+' button in search list (foods). Need to find a way to disable it.

--- client/src/components/food-search-list-item/food-search-list-item.tsx
+++ client/src/components/food-search-list-item/food-search-list-item.tsx
@@ -2,6 +2,9 @@ import { FaCirclePlus } from 'react-icons/fa6';
 import { useFoods } from '../../hooks/useFoods';
 import { IFood } from '../../types/food';
 import { ListItem } from './food-search-list-item.styled';
+import { useMeals } from '../../hooks/useMeals';
+import { IMealComponent } from '../../types/meal-component';
+import { IconType } from 'react-icons';
 
 interface FoodSearchListItemProps {
   food: IFood;
@@ -9,15 +12,25 @@ interface FoodSearchListItemProps {
 export default function FoodSearchListItem(props: FoodSearchListItemProps) {
   const { brand, name, servingSize, calories } = props.food;
   const { handleSelectFood } = useFoods();
+  const { addToMeal } = useMeals();
 
-  function handleClick() {
+  function handleSelectClick() {
     handleSelectFood(props.food);
     console.log(props.food);
   }
 
+  function handleAddToMeal(evt: React.MouseEvent<IconType>) {
+    evt.stopPropagation();
+    const mealComponent: IMealComponent = {
+      food: props.food,
+      servings: 1,
+    };
+    addToMeal(mealComponent);
+  }
+
   return (
-    <ListItem onClick={handleClick}>
-      <FaCirclePlus />
+    <ListItem onClick={handleSelectClick}>
+      <FaCirclePlus onClick={handleAddToMeal} />
       <div>
         {brand} {name}
       </div>
