5e3e7717893958abb0149dc166e678b2fce9d6b6
Jon Mico <jonmico@gmail.com>
2023-12-16 22:38:39 -0500

    Testing to see if this fixed refresh issue.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,7 @@ import AddMeal from './pages/add-meal/add-meal';
 import Logs from './pages/logs-page/logs';
 import CreateFood from './pages/create-food/create-food';
 import GlobalStyles from './styles/global-styles';
+import Home from './pages/home/home';
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route element={<AppLayout />}>
-            <Route index element={<Navigate replace to={'/dashboard'} />} />
+            <Route path={'/'} element={<Home />} />
             <Route path={'/dashboard'} element={<Dashboard />} />
             <Route path={'/add-meal'} element={<AddMeal />} />
             <Route path={'/logs'} element={<Logs />} />
--- client/src/pages/home/home.tsx
+++ client/src/pages/home/home.tsx
@@ -1,7 +1,10 @@
+import { Link } from 'react-router-dom';
+
 export default function Home() {
   return (
     <div>
       <h1>HOME</h1>
+      <Link to={'/dashboard'}>Dashboard</Link>
     </div>
   );
 }
