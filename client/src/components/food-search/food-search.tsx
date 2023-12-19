592482bba9565f67c97555098e2b7b5f2d5db302
Jon Mico <jonmico@gmail.com>
2023-12-18 22:33:37 -0500

    Add isLoading check when rendering food-search-list.

--- client/src/components/food-search/food-search.tsx
+++ client/src/components/food-search/food-search.tsx
@@ -73,7 +73,7 @@ export default function FoodSearch() {
         </SpinnerContainer>
       )}
 
-      {searchedFoods?.length > 0 && (
+      {searchedFoods?.length > 0 && !isLoading && (
         <FoodSearchList>
           {searchedFoods?.map((food) => (
             <FoodSearchListItem key={food._id} food={food} />
