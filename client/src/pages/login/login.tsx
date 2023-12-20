a3bd3700dd5a9100de3c6537521df8dbdc5bdff8
Jon Mico <jonmico@gmail.com>
2023-12-20 12:06:45 -0500

    Hook up login page with state. Login works-ish. I do not think it persists.

--- client/src/pages/login/login.tsx
+++ client/src/pages/login/login.tsx
@@ -1,10 +1,23 @@
+import { useState } from 'react';
 import { PrimaryButton } from '../../components/button/button.styled';
 import styles from './login.module.css';
+import { login } from '../../services/user-api';
+import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
-  function handleSubmit(evt: React.FormEvent<HTMLFormElement>) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  async function handleSubmit(evt: React.FormEvent<HTMLFormElement>) {
     evt.preventDefault();
-    console.log('hello?');
+
+    const data = await login(email, password);
+
+    // TODO: Probably not the best way to reroute.
+    if (data.isValid) {
+      navigate('/dashboard');
+    }
   }
   return (
     <form onSubmit={handleSubmit} className={styles.loginForm}>
@@ -12,11 +25,23 @@ export default function Login() {
       <div className={styles.loginFormFieldContainer}>
         <div className={styles.loginFormField}>
           <label htmlFor='email'>Email</label>
-          <input type='text' name='email' id='email' />
+          <input
+            type='text'
+            name='email'
+            id='email'
+            onChange={(evt) => setEmail(evt.target.value)}
+            value={email}
+          />
         </div>
         <div className={styles.loginFormField}>
           <label htmlFor='password'>Password</label>
-          <input type='password' name='password' id='password' />
+          <input
+            type='password'
+            name='password'
+            id='password'
+            onChange={(evt) => setPassword(evt.target.value)}
+            value={password}
+          />
         </div>
         <PrimaryButton>Login</PrimaryButton>
       </div>
