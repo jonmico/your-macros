d31b9346966503eabd185ad7f78d14433ba3a185
Jon Mico <jonmico@gmail.com>
2023-12-26 21:38:55 -0500

    Style close button for modal.

--- client/src/components/add-meal-to-log-modal/add-meal-to-log-modal.tsx
+++ client/src/components/add-meal-to-log-modal/add-meal-to-log-modal.tsx
@@ -1,5 +1,6 @@
 import styles from './add-meal-to-log-modal.module.css';
 import { createPortal } from 'react-dom';
+import { FaXmark } from 'react-icons/fa6';
 
 interface SetShowModalProps {
   setShowModal: React.Dispatch<React.SetStateAction<boolean>>;
@@ -9,8 +10,15 @@ export default function AddMealToLogModal({ setShowModal }: SetShowModalProps) {
   return createPortal(
     <div className={styles.modalContainer}>
       <div className={styles.modal}>
-        <button onClick={() => setShowModal(false)}>CLOSE</button>
-        <h1>HOWDY</h1>
+        <div className={styles.modalHeader}>
+          <h3>Select the log:</h3>
+          <button
+            className={styles.closeButton}
+            onClick={() => setShowModal(false)}
+          >
+            <FaXmark />
+          </button>
+        </div>
       </div>
     </div>,
     document.body
