fd54897de6bdfbf84f9cb17673ea25d3f5b7b353
Jon Mico <jonmico@gmail.com>
2024-01-01 23:35:54 -0500

    Start dashboard. Honestly have no clue how to begin the layout for this.

--- client/src/pages/dashboard/dashboard.tsx
+++ client/src/pages/dashboard/dashboard.tsx
@@ -1,7 +1,19 @@
+import PageHeader from '../../components/page-header/page-header';
+import PageContentContainer from '../../components/page-content/page-content-container';
+import useUser from '../../hooks/useUser';
+import MacroDisplay from '../../components/macro-display/macro-display';
+
 export default function Dashboard() {
+  const { user } = useUser();
+
+  if (!user) return null;
+
   return (
     <div>
-      <h1>DASHBOARD</h1>
+      <PageHeader>Dashboard</PageHeader>
+      <PageContentContainer>
+        <MacroDisplay calories={user.calories} macros={user.macros} />
+      </PageContentContainer>
     </div>
   );
 }
