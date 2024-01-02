89e68a8b8086d1fa649e1db243543b0cb6653245
Jon Mico <jonmico@gmail.com>
2024-01-01 23:49:26 -0500

    Add MacrosAndCalories to Dashboard page. Trying a new file structure.

--- client/src/pages/dashboard/dashboard.tsx
+++ client/src/pages/dashboard/dashboard.tsx
@@ -1,19 +1,44 @@
-import PageHeader from '../../components/page-header/page-header';
 import PageContentContainer from '../../components/page-content/page-content-container';
+import PageHeader from '../../components/page-header/page-header';
 import useUser from '../../hooks/useUser';
-import MacroDisplay from '../../components/macro-display/macro-display';
+import { IMacros } from '../../types/macros';
+
+import styles from './dashboard.module.css';
 
 export default function Dashboard() {
-  const { user } = useUser();
-
-  if (!user) return null;
-
   return (
     <div>
       <PageHeader>Dashboard</PageHeader>
+      <DashboardContent />
+    </div>
+  );
+}
+
+function DashboardContent() {
+  const { user } = useUser();
+
+  if (!user) return null;
+  return (
+    <>
       <PageContentContainer>
-        <MacroDisplay calories={user.calories} macros={user.macros} />
+        <MacrosAndCalories calories={user.calories} macros={user.macros} />
       </PageContentContainer>
+    </>
+  );
+}
+
+interface MacrosAndCaloriesProps {
+  calories: number;
+  macros: IMacros;
+}
+
+function MacrosAndCalories(props: MacrosAndCaloriesProps) {
+  return (
+    <div className={styles.macrosAndCaloriesContainer}>
+      <div>{props.calories}</div>
+      <div>{props.macros.fat}</div>
+      <div>{props.macros.carbs}</div>
+      <div>{props.macros.protein}</div>
     </div>
   );
 }
