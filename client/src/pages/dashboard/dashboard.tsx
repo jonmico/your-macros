f8e109ab750c0b2d958f71cc31d1c84af5356550
Jon Mico <jonmico@gmail.com>
2024-01-02 20:54:13 -0500

    Early dashboard table.

--- client/src/pages/dashboard/dashboard.tsx
+++ client/src/pages/dashboard/dashboard.tsx
@@ -1,8 +1,9 @@
+import { useState } from 'react';
 import PageContentContainer from '../../components/page-content/page-content-container';
 import PageHeader from '../../components/page-header/page-header';
 import useUser from '../../hooks/useUser';
-import { IMacros } from '../../types/macros';
 
+import { IUser } from '../../types/user';
 import styles from './dashboard.module.css';
 
 export default function Dashboard() {
@@ -17,28 +18,61 @@ export default function Dashboard() {
 function DashboardContent() {
   const { user } = useUser();
 
-  if (!user) return null;
+  if (!user || user.logs.length === 0) return null;
+
   return (
     <>
       <PageContentContainer>
-        <MacrosAndCalories calories={user.calories} macros={user.macros} />
+        <h3>Howdy!</h3>
+        <DashboardTable user={user} />
       </PageContentContainer>
     </>
   );
 }
 
-interface MacrosAndCaloriesProps {
-  calories: number;
-  macros: IMacros;
-}
+function DashboardTable(props: { user: IUser }) {
+  const [selectedLog, setSelectedLog] = useState(props.user.logs[0]);
 
-function MacrosAndCalories(props: MacrosAndCaloriesProps) {
+  const remainingCalories = props.user.calories - selectedLog.calories;
+  const remainingFat = props.user.macros.fat - selectedLog.macros.fat;
+  const remainingCarbs = props.user.macros.carbs - selectedLog.macros.carbs;
+  const remainingProtein =
+    props.user.macros.protein - selectedLog.macros.protein;
+
   return (
-    <div className={styles.macrosAndCaloriesContainer}>
-      <div>{props.calories}</div>
-      <div>{props.macros.fat}</div>
-      <div>{props.macros.carbs}</div>
-      <div>{props.macros.protein}</div>
+    <div className={styles.dashboardContainer}>
+      <div className={styles.dashboardTableIntro}>
+        <h4>
+          Here is where you're at for your{' '}
+          <span className={styles.selectedLog}>{selectedLog.name}</span> log:
+        </h4>
+        <div>dropdown menu</div>
+      </div>
+      <div className={styles.dashboardTableContainer}>
+        <div className={styles.dashboardTable}>
+          <div className={styles.dashboardTableRow}>
+            <div>Initial:</div>
+            <div className={styles.number}>{props.user.calories}cals</div>
+            <div className={styles.number}>{props.user.macros.fat}f</div>
+            <div className={styles.number}>{props.user.macros.carbs}c</div>
+            <div className={styles.number}>{props.user.macros.protein}p</div>
+          </div>
+          <div className={styles.dashboardTableRow}>
+            <div>Consumed:</div>
+            <div className={styles.number}>{selectedLog.calories}cals</div>
+            <div className={styles.number}>{selectedLog.macros.fat}f</div>
+            <div className={styles.number}>{selectedLog.macros.carbs}c</div>
+            <div className={styles.number}>{selectedLog.macros.protein}p</div>
+          </div>
+          <div className={styles.dashboardTableRow}>
+            <div>Remaining:</div>
+            <div className={styles.number}>{remainingCalories}cals</div>
+            <div className={styles.number}>{remainingFat}f</div>
+            <div className={styles.number}>{remainingCarbs}c</div>
+            <div className={styles.number}>{remainingProtein}p</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
