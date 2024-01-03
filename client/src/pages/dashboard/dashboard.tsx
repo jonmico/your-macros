b3c90ba581cb76354f231579444eb47dfd623bdd
Jon Mico <jonmico@gmail.com>
2024-01-02 23:46:40 -0500

    Add Dropdown Menu.

--- client/src/pages/dashboard/dashboard.tsx
+++ client/src/pages/dashboard/dashboard.tsx
@@ -2,9 +2,10 @@ import { useState } from 'react';
 import PageContentContainer from '../../components/page-content/page-content-container';
 import PageHeader from '../../components/page-header/page-header';
 import useUser from '../../hooks/useUser';
-
+import { FaAngleLeft } from 'react-icons/fa6';
 import { IUser } from '../../types/user';
 import styles from './dashboard.module.css';
+import { ILog } from '../../types/log';
 
 export default function Dashboard() {
   return (
@@ -23,7 +24,7 @@ function DashboardContent() {
   return (
     <>
       <PageContentContainer>
-        <h3>Howdy!</h3>
+        {/* <h3>Howdy!</h3> */}
         <DashboardTable user={user} />
       </PageContentContainer>
     </>
@@ -31,13 +32,19 @@ function DashboardContent() {
 }
 
 function DashboardTable(props: { user: IUser }) {
-  const [selectedLog, setSelectedLog] = useState(props.user.logs[0]);
+  const [selectedLog, setSelectedLog] = useState(
+    props.user.logs[props.user.logs.length - 1]
+  );
 
   const remainingCalories = props.user.calories - selectedLog.calories;
   const remainingFat = props.user.macros.fat - selectedLog.macros.fat;
   const remainingCarbs = props.user.macros.carbs - selectedLog.macros.carbs;
   const remainingProtein =
     props.user.macros.protein - selectedLog.macros.protein;
+
+  function handleSelectLog(log: ILog) {
+    setSelectedLog(log);
+  }
 
   return (
     <div className={styles.dashboardContainer}>
@@ -46,13 +53,17 @@ function DashboardTable(props: { user: IUser }) {
           Here is where you're at for your{' '}
           <span className={styles.selectedLog}>{selectedLog.name}</span> log:
         </h4>
-        <div>dropdown menu</div>
+        <DropDownMenu
+          selectedLog={selectedLog}
+          logs={props.user.logs}
+          handleSelectLog={handleSelectLog}
+        />
       </div>
       <div className={styles.dashboardTableContainer}>
         <div className={styles.dashboardTable}>
           <div className={styles.dashboardTableRow}>
             <div>Initial:</div>
-            <div className={styles.number}>{props.user.calories}cals</div>
+            <div className={styles.initialCals}>{props.user.calories}cals</div>
             <div className={styles.number}>{props.user.macros.fat}f</div>
             <div className={styles.number}>{props.user.macros.carbs}c</div>
             <div className={styles.number}>{props.user.macros.protein}p</div>
@@ -73,6 +84,34 @@ function DashboardTable(props: { user: IUser }) {
           </div>
         </div>
       </div>
+    </div>
+  );
+}
+
+function DropDownMenu(props: {
+  selectedLog: ILog;
+  logs: ILog[];
+  handleSelectLog: (log: ILog) => void;
+}) {
+  const [isDropDownMenuOpen, setIsDropDownMenuOpen] = useState(false);
+
+  return (
+    <div className={styles.dropDownMenu}>
+      <button
+        onClick={() => setIsDropDownMenuOpen((prevState) => !prevState)}
+        className={styles.dropDownButton}
+      >
+        <div className={styles.dropDownMenuSelectedLog}>
+          {props.selectedLog.name}
+        </div>
+        <div className={styles.dropDownIconContainer}>
+          <FaAngleLeft
+            className={`${
+              isDropDownMenuOpen ? styles.animateDropDownButton : ''
+            }`}
+          />
+        </div>
+      </button>
     </div>
   );
 }
