80adef29955155cb4974fc0849b5a4a871f40057
Jon Mico <jonmico@gmail.com>
2024-01-08 14:37:39 -0500

    Early list for food builder.

--- client/src/components/your-food-form-food-builder/your-food-form-food-builder.tsx
+++ client/src/components/your-food-form-food-builder/your-food-form-food-builder.tsx
@@ -1,9 +1,12 @@
+import { useState } from 'react';
 import { IFood } from '../../types/food';
 import styles from './your-food-form-food-builder.module.css';
 
 export default function YourFoodFormFoodBuilder(props: {
   foodComponents: { food: IFood; servings: number }[];
 }) {
+  const [yourFoodName, setYourFoodName] = useState('');
+
   return (
     <div className={styles.yourFoodFormBuilderContainer}>
       {props.foodComponents.length === 0 ? (
@@ -12,12 +15,45 @@ export default function YourFoodFormFoodBuilder(props: {
           <p>As you add food items, they will show up here.</p>
         </div>
       ) : (
-        <ul>
-          {props.foodComponents.map((foodComponent) => (
-            <li key={foodComponent.food._id}>{foodComponent.food.name}</li>
-          ))}
-        </ul>
+        <>
+          <div className={styles.mealNameInputContainer}>
+            <label className={styles.mealNameLabel} htmlFor='yourFoodName'>
+              YourFood Name
+            </label>
+            <input
+              className={styles.mealNameInput}
+              placeholder={'my favorite dish, etc'}
+              type='text'
+              name={'yourFoodName'}
+              id={'yourFoodName'}
+              value={yourFoodName}
+              onChange={(evt) => setYourFoodName(evt.target.value)}
+            />
+          </div>
+          <div className={styles.formBuilderListContainer}>
+            <ul className={styles.formBuilderList}>
+              {props.foodComponents.map((foodComponent) => (
+                <YourFoodFormBuilderListItem
+                  key={foodComponent.food._id}
+                  foodComponent={foodComponent}
+                />
+              ))}
+            </ul>
+          </div>
+        </>
       )}
     </div>
+  );
+}
+
+function YourFoodFormBuilderListItem(props: {
+  foodComponent: { food: IFood; servings: number };
+}) {
+  return (
+    <li className={styles.formBuilderListItem}>
+      <div>{props.foodComponent.food.name}</div>
+      <div>{props.foodComponent.servings}</div>
+      <div>{props.foodComponent.food.calories}</div>
+    </li>
   );
 }
