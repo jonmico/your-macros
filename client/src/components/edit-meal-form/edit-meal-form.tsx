a02edbcbcd2e99b2c429a8cf3f02abf95eeee657
Jon Mico <jonmico@gmail.com>
2024-02-02 23:13:29 -0500

    Implement removeFromMeal function in EditMealForm.

--- client/src/components/edit-meal-form/edit-meal-form.tsx
+++ client/src/components/edit-meal-form/edit-meal-form.tsx
@@ -3,12 +3,32 @@ import styles from './edit-meal-form.module.css';
 import { IMeal } from '../../types/meal';
 import { useState } from 'react';
 import { IMealComponent } from '../../types/meal-component';
+import { calcCaloriesMacros } from '../../utils/calcCaloriesMacros';
 
 export default function EditMealForm(props: {
   handleCloseModal: () => void;
   mealToEdit: IMeal;
 }) {
   const [mealToEditCopy, setMealToEditCopy] = useState({ ...props.mealToEdit });
+
+  function removeFromMeal(mealComponentId: string) {
+    const filteredMealComponents = mealToEditCopy.mealComponents.filter(
+      (mealComp) => mealComp._id !== mealComponentId
+    );
+
+    const { totalCals, totalCarbs, totalFat, totalProtein } =
+      calcCaloriesMacros(filteredMealComponents);
+
+    const updatedMeal: IMeal = {
+      ...mealToEditCopy,
+      mealComponents: filteredMealComponents,
+      calories: totalCals,
+      macros: { carbs: totalCarbs, fat: totalFat, protein: totalProtein },
+    };
+
+    setMealToEditCopy(updatedMeal);
+  }
+
   return (
     <form className={styles.editMealForm}>
       <div className={styles.editMealFormHeader}>
@@ -23,6 +43,7 @@ export default function EditMealForm(props: {
             <EditMealFormMealComponentListItem
               key={mealComp._id}
               mealComp={mealComp}
+              removeFromMeal={removeFromMeal}
             />
           ))}
         </ul>
@@ -33,8 +54,11 @@ export default function EditMealForm(props: {
 
 function EditMealFormMealComponentListItem(props: {
   mealComp: IMealComponent;
+  removeFromMeal: (mealComponentId: string) => void;
 }) {
   const { mealComp } = props;
+  const [servings, setServings] = useState(String(mealComp.servings));
+
   return (
     <li className={styles.mealComponentListItem}>
       <div>
@@ -43,11 +67,20 @@ function EditMealFormMealComponentListItem(props: {
       </div>
       <div>
         <label htmlFor='servings'>Servings</label>
-        <input type='number' id={'servings'} name={'servings'} />
+        <input
+          value={servings}
+          onChange={(evt) => setServings(evt.target.value)}
+          type='number'
+          id={'servings'}
+          name={'servings'}
+        />
       </div>
       <div>macro info</div>
       <div>
-        <button>
+        <button
+          type={'button'}
+          onClick={() => props.removeFromMeal(mealComp._id)}
+        >
           <FaXmark />
         </button>
       </div>
