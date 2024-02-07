aeb2bca6b047275d8a2fbb6b29c0c10ad6d0944c
Jon Mico <jonmico@gmail.com>
2024-02-06 21:53:16 -0500

    Add containers for styling purposes.

--- client/src/components/edit-meal-form/edit-meal-form.tsx
+++ client/src/components/edit-meal-form/edit-meal-form.tsx
@@ -73,7 +73,7 @@ export default function EditMealForm(props: {
   }
 
   return (
-    <div>
+    <div className={styles.editMealFormContainer}>
       <form className={styles.editMealForm}>
         <div className={styles.editMealFormHeader}>
           <h2>Edit Meal</h2>
