4a65ee071c52a8eda19f64eddc427e4f4f2458d2
Jon Mico <jonmico@gmail.com>
2024-02-09 19:01:16 -0500

    Add state to edit-meal-context to improve adding foods to a meal when editing.

--- client/src/contexts/edit-meal-context.tsx
+++ client/src/contexts/edit-meal-context.tsx
@@ -1,10 +1,15 @@
 import { SetStateAction, createContext, useState } from 'react';
 import { IMeal } from '../types/meal';
+import { IFood } from '../types/food';
 
 interface IEditMealContext {
   mealToEdit: IMeal | null;
   setMealToEdit: React.Dispatch<SetStateAction<IMeal | null>>;
   logId: string;
+  searchInput: string;
+  setSearchInput: React.Dispatch<SetStateAction<string>>;
+  searchedFoods: IFood[];
+  setSearchedFoods: React.Dispatch<SetStateAction<IFood[]>>;
 }
 
 export const EditMealContext = createContext<IEditMealContext | null>(null);
@@ -14,8 +19,18 @@ export function EditMealProvider(props: {
   logId: string;
 }) {
   const [mealToEdit, setMealToEdit] = useState<IMeal | null>(null);
+  const [searchedFoods, setSearchedFoods] = useState<IFood[]>([]);
+  const [searchInput, setSearchInput] = useState('');
 
-  const value = { mealToEdit, setMealToEdit, logId: props.logId };
+  const value = {
+    mealToEdit,
+    setMealToEdit,
+    logId: props.logId,
+    searchedFoods,
+    setSearchedFoods,
+    searchInput,
+    setSearchInput,
+  };
 
   return (
     <EditMealContext.Provider value={value}>
