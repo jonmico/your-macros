a7020a62045e037c77e34692589b2821899ccf4a
Jon Mico <jonmico@gmail.com>
2024-02-15 20:38:09 -0500

    Change some layout/styling.

--- client/src/pages/search-foods/search-foods.tsx
+++ client/src/pages/search-foods/search-foods.tsx
@@ -8,6 +8,10 @@ export default function SearchFoods() {
   const [searchInput, setSearchInput] = useState('');
   const [searchedFoods, setSearchedFoods] = useState<IFood[]>([]);
 
+  const foodsList = searchedFoods.map((searchedFood) => (
+    <SearchedFoodsListItem key={searchedFood._id} food={searchedFood} />
+  ));
+
   async function handleSubmit(evt: React.FormEvent<HTMLFormElement>) {
     evt.preventDefault();
     const data: { foods: IFood[] } = await getFoodByText(searchInput);
@@ -16,15 +20,18 @@ export default function SearchFoods() {
   }
   return (
     <div className={styles.searchFoodsContainer}>
-      <h2>Browse the database for foods</h2>
-      <form onSubmit={handleSubmit}>
-        <SearchBar searchInput={searchInput} setSearchInput={setSearchInput} />
-        <ul>
-          {searchedFoods.map((searchedFood) => (
-            <SearchedFoodsListItem key={searchedFood._id} food={searchedFood} />
-          ))}
-        </ul>
-      </form>
+      <h2 className={styles.searchFoodsHeader}>
+        Browse the database for foods
+      </h2>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <SearchBar
+            searchInput={searchInput}
+            setSearchInput={setSearchInput}
+          />
+        </form>
+        <ul className={styles.searchFoodsList}>{foodsList}</ul>
+      </div>
     </div>
   );
 }
